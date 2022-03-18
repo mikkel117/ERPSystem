16e606841f8bd33c2439d99dda5a7d1b0e74af7a
rumbo <rumbo.9370@gmail.com>
2022-03-18 13:55:44 +0100

    D1: Company class

---
+++ SupremEPRSystemForRealBussiness/Model/Company.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupremEPRSystemForRealBussiness.Model
+{
+    internal class Company
+    {
+        public string CompanyName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Currency { get; set; }
+    }
+}

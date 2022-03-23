c20776c9c659a46c17f5a1a44772934d128b7d86
Rasmusbb <56928150+Rasmusbb@users.noreply.github.com>
2022-03-23 13:53:56 +0100

    hello wolrd

--- SupremEPRSystemForRealBussiness/Depricated/Data/Database.cs
+++ SupremEPRSystemForRealBussiness/Depricated/Data/Database.cs
@@ -9,6 +9,7 @@ namespace SupremEPRSystemForRealBussiness.Data
     partial class Database
     {
         public static Database Instance { get; private set; }
+        public List<src.Customer> Customers  { get; set; }
         static Database()
         {
             Instance = new Database();

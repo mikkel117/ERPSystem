0cc40aa38393d0fe7a2cd0137a5faddbd27e0b56
Lars Rosenkilde <lars.rosenkilde95@gmail.com>
2022-03-28 09:12:13 +0200

    Updated Person class

--- SupremEPRSystemForRealBussiness/Program.cs
+++ SupremEPRSystemForRealBussiness/Program.cs
@@ -1,43 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using TECHCOOL.UI;
+using SupremEPRSystemForRealBussiness.src.Menus;
 
-namespace SupremEPRSystemForRealBussiness.src {
-    public class Address {
-        public Address(string streetName, string city, string country, int zipCode, string region) {
-            City = city;
-            StreetName = streetName;
-            Country = country;
-            ZipCode = zipCode;
-            Region = region;
-        }
-
-        private string City {
-            get { return City; }
-            set { City = value ?? "Not entered.."; }
-        }
-        private string StreetName {
-            get { return StreetName; }
-            set { StreetName = value ?? "Not entered.."; }
-        }
-        private string Floor {
-            get { return Floor; }
-            set { Floor = value ?? "Not entered.."; }
-        }
-        private string Country {
-            get { return Country; }
-            set { Country = value ?? "Not entered.."; }
-        }
-        private int ZipCode {
-            get { return ZipCode; }
-            set { ZipCode = value; }
-        }
-        private string Region {
-            get { return Region; }
-            set { Region = value ?? "Not entered.."; }
-        }
-
+namespace SupremEPRSystemForRealBussiness;
+class SupremEPRSystem
+{
+    static void Main(string[] args)
+    {
+        Screen.Display(new MainMenu());
     }
-
 }

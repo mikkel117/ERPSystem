9f5e1c22ff9fbd060ecacc8b7909cb60ee2cd09c
rumbo <rumbo.9370@gmail.com>
2022-04-04 13:51:56 +0200

    update

--- SupremEPRSystemForRealBussiness/src/Data/Database.cs
+++ SupremEPRSystemForRealBussiness/src/Data/Database.cs
@@ -9,7 +9,6 @@ namespace SupremEPRSystemForRealBussiness.Data
     partial class Database
     {
         public static Database Instance { get; private set; }
-        public List<src.Customer> Customers { get; set; }
         static Database()
         {
             Instance = new Database();

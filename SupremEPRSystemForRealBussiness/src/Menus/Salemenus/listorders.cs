bc4cdd3b040eeeb6e60c15dd7d8a3167012ea762
Rasmusbb <56928150+Rasmusbb@users.noreply.github.com>
2022-05-03 11:06:30 +0200

    F2 Genvej added

--- SupremEPRSystemForRealBussiness/src/Menus/Salemenus/listorders.cs
+++ SupremEPRSystemForRealBussiness/src/Menus/Salemenus/listorders.cs
@@ -15,22 +15,38 @@ namespace SupremEPRSystemForRealBussiness.src.Menus
         protected override void Draw()
         {
             Clear(this);
+
             ListPage<SalesOrder> listPage = new ListPage<SalesOrder>();
+            Console.WriteLine("F2 Opret ny order");
+            listPage.AddKey(ConsoleKey.F2, neworder);
             listPage.AddColumn("Order", "Orderid");
             listPage.AddColumn("Name", "Name");
             listPage.AddColumn("CustomerId", "CustomerId");
             listPage.AddColumn("Dato", "TimeStamp");
             listPage.AddColumn("Price", "price");
             listPage.Add(Data.Database.Instance.salesOrders);
-            SalesOrder selected = listPage.Select();
+            if(Data.Database.Instance.salesOrders.Count != 0)
+            {
+                SalesOrder selected = listPage.Select();
+            }
+            else
+            {
+                neworder(new());
+            }
+            
+            
         }
         void orderdetails(SalesOrder sel)
         {
             Quit();
             Console.Clear();
             Console.WriteLine("Order Details");
-           
-
+        }
+        void neworder(SalesOrder _)
+        {
+            Quit();
+            CreateOrder createOrder = new CreateOrder();
+            Screen.Display(createOrder);
         }
     }
 }

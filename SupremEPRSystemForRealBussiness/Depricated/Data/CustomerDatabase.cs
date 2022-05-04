edef5f05adf83314080f8779ddcc5ef5170f3372
rumbo <rumbo.9370@gmail.com>
2022-05-04 15:09:17 +0200

    update to customer database can get customerbyid

--- SupremEPRSystemForRealBussiness/Depricated/Data/CustomerDatabase.cs
+++ SupremEPRSystemForRealBussiness/Depricated/Data/CustomerDatabase.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +15,9 @@ namespace SupremEPRSystemForRealBussiness.Data
 
         public List<Customer> SelectCustomer()
         {
-                return customers;
+            Debug.WriteLine(connection.State.ToString());
+            Customer test = GetCustomerById();
+            return customers;
         }
 
         public void InsertCustomer(Customer customer)
@@ -26,7 +30,26 @@ namespace SupremEPRSystemForRealBussiness.Data
             {
                 return true;
             }
-            else { return false; }  
+            else { return false; }
+        }
+
+        public Customer? GetCustomerById(int Id = 6)
+        {
+            SqlDataReader dr;
+            SqlCommand scmd = connection.CreateCommand();
+            scmd.CommandText = "SELECT CustomerId, FirstName, LastName, AddressId, ContactInfoId, LastOrderDate FROM Customer WHERE CustomerId = @id";
+            scmd.Parameters.AddWithValue("@id", Id);
+            dr = scmd.ExecuteReader();
+            if (dr.Read())
+            {
+                var CustomerId = dr.GetInt32(0);
+                var FirstName = dr.GetString(1);
+                var LastName = dr.GetString(2);
+                var ContactId = dr.GetInt32(3);
+                var AddressId = dr.GetInt32(4);
+                var LastOrderDate = dr.GetDateTime(5);
+            }
+            return null;
         }
     }
 }
